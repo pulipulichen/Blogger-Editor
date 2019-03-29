cbb2053c830b6ea5dd9d95e5cc7358041c590ff7
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-03-29 20:12:12 +0800

    0328 #14 hajimaru

---
+++ src/views/ImageReplacer/ImageReplacer.js
@@ -0,0 +1,32 @@
+import dayjs from 'dayjs'
+
+var config = {
+  //name: "main-content",
+  data: function () {
+    return {
+      ui: undefined,
+    }
+  },
+  created: function () {
+    $v.ImageReplacer = this
+  },
+  methods: {
+    getUI: function () {
+      if (typeof(this.ui) === 'undefined') {
+        //console.log('find ui')
+        this.ui = this.ui = $('.EditorManager.ui.modal')
+      }
+      return this.ui
+    },
+    open: function () {
+      //console.log(this.data)
+      this.getUI().modal('show')
+    },
+    close: function () {
+      this.getUI().modal('hide')
+    },
+  }
+}
+
+//window.EditorManager = EditorManager
+export default config
---
+++ src/views/ImageReplacer/ImageReplacer.vue
@@ -0,0 +1,3 @@
+<template src="./ImageReplacer.html"></template>
+<script src="./ImageReplacer.js"></script>
+<style src="./ImageReplacer.less" scoped></style>
