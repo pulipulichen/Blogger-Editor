19ffabb0cf56456ddddd0cb2b3c602b3b5e3062a
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-04-01 16:12:05 +0800

    0331 #24 clean codes, part 2

--- src/footer.js
+++ src/footer.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 
-//require('./vendor/garlic/garlic.min.js')
-require('./vendor/dayjs/dayjs.min.js')
+require('./helpers/ConfigHelper.js')
 
-//require('./vendor/popper/popper.min.js')
-//require('./vendor/bootstrap/js/bootstrap.js')
+//require('./vendor/garlic/garlic.min.js')
+//require('./vendor/dayjs/dayjs.min.js')
 
 require('./helpers/FunctionHelper.js')
 require('./helpers/CopyPasteHelper.js')
@@ -21,49 +20,24 @@ require('./helpers/DayjsHelper.js')
 require('./helpers/SemanticUIHelper.js')
 require('./helpers/FileHelper.js')
 
-
-require('./helpers/ConfigHelper.js')
-
-//TemplateManager = import('./views/ThemeManager/TemplateManager.js')
-
 window.$v = {}
 
 import PageLoaderSfc from './views/PageLoader/PageLoader.vue'
 VueHelper.init(PageLoaderSfc)
 
 import BrowserDetectorComponent from './views/BrowserDetector/BrowserDetector.vue'
-//new Vue({
-//  el: "#BrowserDetector",
-//  render: h => h(BrowserDetectorComponent),
-//})
 VueHelper.init(BrowserDetectorComponent)
 
 import PostManagerComponent from './views/PostManager/PostManager.vue'
-//new Vue({
-//  el: "#PostManager",
-//  render: h => h(PostManagerComponent),
-//})
 VueHelper.init(PostManagerComponent)
 
 import ThemeManagerComponent from './views/ThemeManager/ThemeManager.vue'
-//new Vue({
-//  el: "#ThemeManager",
-//  render: h => h(ThemeManagerComponent),
-//})
 VueHelper.init(ThemeManagerComponent)
 
 import EditorManagerComponent from './views/EditorManager/EditorManager.vue'
-//new Vue({
-//  el: "#EditorManager",
-//  render: h => h(EditorManagerComponent),
-//})
 VueHelper.init(EditorManagerComponent)
 
 import NavBarComponent from './views/NavBar/NavBar.vue'
-//new Vue({
-//  el: "#NavBar",
-//  render: h => h(NavBarComponent),
-//})
 VueHelper.init(NavBarComponent)
 
 // --------------------
