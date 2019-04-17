22ec6f0b1d12ae08c444c10ca14cd7fc64f4c735
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-04-17 14:36:17 +0800

    0417 #70 go

---
+++ src/views/EditorManager/SnippetInserter/SnippetInserter.js
@@ -0,0 +1,38 @@
+let config = {
+  data: function () {
+    return {
+      name: 'SnippetInserter',
+      ui: undefined,
+    }
+  },
+  mounted: function () {
+    
+  },
+  computed: {
+    
+  },
+  created: function () {
+    $v[this.name] = this
+  },
+  methods: {
+    // ---------------------
+    // Methods of Modal
+    // ---------------------
+    
+    getUI: function () {
+      if (typeof(this.ui) === 'undefined') {
+        //console.log('find ui')
+        this.ui = $(this.$refs.modal)
+      }
+      return this.ui
+    },
+    open: function () {
+      this.getUI().modal('show')
+    },
+    close: function () {
+      this.getUI().modal('hide')
+    }
+  }
+}
+
+export default config
