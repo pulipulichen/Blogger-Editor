98f0f4ac54a727acc3e5557594728e03bf1afcd0
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-06-20 16:59:45 +0800

    0620 #158 真神奇

--- bin/index-electron.js
+++ bin/index-electron.js
@@ -10,6 +10,16 @@ const {
   BrowserWindow,
 } = require('electron')
 
+const contextMenu = require('electron-context-menu');
+
+contextMenu({
+	prepend: (defaultActions, params, browserWindow) => [{
+		label: 'Rainbow',
+		// Only show it when right-clicking images
+		visible: params.mediaType === 'image'
+	}]
+});
+
 // ------------
 
 app.on('ready', createWindow)
