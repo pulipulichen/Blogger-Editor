0f460bcbb54eea1d9174623ac4b13f2f1e3909b6
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-06-26 16:28:11 +0800

    0625 #162 我到底要不要開新的package專案呢...

--- static/Electron/ElectronHelper.js
+++ static/Electron/ElectronHelper.js
@@ -6,40 +6,44 @@ let ElectronHelper = {
     }
   },
   isElectronEnvironment: function () {
-    return (typeof(process) === 'object' 
-            && typeof(process.versions) === 'object'
-            && typeof(process.versions['electron']) === 'string')
+    return (typeof (process) === 'object'
+            && typeof (process.versions) === 'object'
+            && typeof (process.versions['electron']) === 'string')
   },
   filterProtocol: function (url) {
-    if (typeof(url) === 'string' && url.startsWith('//')) {
+    if (typeof (url) === 'string' && url.startsWith('//')) {
       if (this.isElectronEnvironment()) {
         return 'http:' + url
-      }
-      else {
+      } else {
         return url
       }
-    }
-    else {
+    } else {
       return url
     }
   },
   initElectronInPageSearch: function () {
     const searchInPage = require('electron-in-page-search').default;
     const remote = require('electron').remote;
-   
+
     // Create an instance with the current window
-        let searcher = remote.getCurrentWebContents()
-        //console.log(typeof(searcher.send))
-        //searcher.send = () => {}
-        const inPageSearch = searchInPage(searcher);
-        
-        // Alternatively add the key event listener [CTRL+F]
-        window.addEventListener("keydown", (e) => {
-            if ((e.ctrlKey || e.metaKey) && e.keyCode === 70) {
-                inPageSearch.openSearchWindow();
-            }
-        }, false);
-    
+    let searcher = remote.getCurrentWebContents()
+    //console.log(typeof(searcher.send))
+    //searcher.send = () => {}
+    const inPageSearch = searchInPage(searcher);
+
+    // Alternatively add the key event listener [CTRL+F]
+    window.addEventListener("keydown", (e) => {
+      // 這裡是呼叫的地方
+      if ((e.ctrlKey || e.metaKey) && e.keyCode === 70) {
+        try {
+          inPageSearch.openSearchWindow();
+        }
+        catch (e) {
+          console.error(e)
+        }
+      }
+    }, false);
+
   }
 }
 
