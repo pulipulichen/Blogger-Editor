cc3e07ed6caca3f709500a8ed7e4ed051f55dbe4
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-07-10 21:58:13 +0800

    0709 #200 來開始囉

---
+++ src/views/EditorManager/GoogleDocLinkBuilder/GoogleDocLinkBuilder.vue
@@ -0,0 +1,4 @@
+<template src="./GoogleDocLinkBuilder.html"></template>
+<script src="./GoogleDocLinkBuilder.js"></script>
+<style src="./GoogleDocLinkBuilder.less" scoped></style>
+<i18n src="./GoogleDocLinkBuilder.json"></i18n>
