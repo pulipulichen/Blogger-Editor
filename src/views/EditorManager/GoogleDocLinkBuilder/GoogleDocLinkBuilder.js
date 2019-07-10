d4142d85954d0cacfe58c6ee8c43338cd15fd52b
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-07-11 01:25:49 +0800

    0709 #200 總之先把語系檔的部分做完了，然後來整合

--- src/views/EditorManager/GoogleDocLinkBuilder/GoogleDocLinkBuilder.js
+++ src/views/EditorManager/GoogleDocLinkBuilder/GoogleDocLinkBuilder.js
@@ -19,25 +19,6 @@ let config = {
     draggable,
   },
   watch: {
-    /*
-    getLinks: function () {
-      setTimeout(() => {
-        this.getUI().find('.ui.checkbox').checkbox()
-      }, 0)
-      return [{
-          url: "https://docs.google.com/presentation/d/1HYPmm0dWobeKUx1j0EEtsrEtuicFHRh4oMWvZHCafL4/edit?usp=sharing",
-          label: "Label A",
-          type: "A",
-          checked: true,
-      },
-      {
-          url: "https://docs.google.com/presentation/d/1HYPmm0dWobeKUx1j0EEtsrEtuicFHRh4oMWvZHCafL4/edit?usp=sharing",
-          label: "Label B",
-          type: "B",
-          checked: false,
-      }]
-    }
-    */
     shareLink: function (shareLink) {
       this.buildLinks(shareLink)
     }
@@ -58,7 +39,7 @@ let config = {
       return this.ui
     },
     init: function () {
-      this.buildLinks()
+      this.buildLinks(this.shareLink)
     },
     open: function () {
       this.getUI().modal('show')
@@ -73,9 +54,25 @@ let config = {
       CopyPasteHelper.copyPlainText(url)
     },
     buildLinks: function (shareLink) {
+      if (typeof(shareLink) !== 'string') {
+        return
+      }
+      
       setTimeout(() => {
         this.getUI().find('.ui.checkbox').checkbox()
       }, 0)
+      
+      let startPos = shareLink.indexOf('/d/')
+      if (startPos > 0) {
+        // Google Drive 模式
+        let id = this.getGoogleDocId(shareLink)
+        let type = this.getGoogleDocType(shareLink)
+        // ---------------------
+        this.links = this.buildGoogleDocLinks(type, id)
+        // ---------------------
+      }
+      
+      /*
       this.links = [{
           url: "https://docs.google.com/presentation/d/1HYPmm0dWobeKUx1j0EEtsrEtuicFHRh4oMWvZHCafL4/edit?usp=sharing",
           label: "Label A",
@@ -88,8 +85,97 @@ let config = {
           type: "B",
           checked: false,
       }]
+      */
+    },
+    getGoogleDocId: function (shareLink) {
+      let startPos = shareLink.indexOf('/d/')
+      if (startPos > 0) {
+        // Google Drive 模式
+        startPos = startPos + 3
+        let endPos = shareLink.indexOf('/', startPos)
+        let id = shareLink.slice(startPos, endPos)
+        return id
+      }
+    },
+    getGoogleDocType: function (_link) {
+      var _type = "file";
+      if (_link.startsWith("https://docs.google.com/document/d/") === true) {
+          _type = "document";
+      } else if (_link.startsWith("https://docs.google.com/spreadsheets/d/") === true) {
+          _type = "spreadsheets";
+      } else if (_link.startsWith("https://docs.google.com/presentation/d/") === true) {
+          _type = "presentation";
+      }
+      return _type
+    },
+    buildGoogleDocLink: function (url, type, icon) {
+      return {
+          url: url,
+          label: this.$t('type.' + type),
+          type: type,
+          icon: icon,
+          checked: false,
+      }
+    },
+    buildGoogleDocLinks: function (type, id) {
+      let links = []
+      let linkPrefix
+      switch (type) {
+        case "document":
+          linkPrefix = "https://docs.google.com/document/d/"
+
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/edit?usp=sharing', type + '.share', "linkify"))
+
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/preview', type + '.preview', "play circle"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/copy', type + '.copy', "copy"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=pdf', type + '.pdf', "file pdf outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=doc', type + '.doc', "file word outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=odt', type + '.odt', "file alternate"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=rtf', type + '.rtf', "file alternate"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=txt', type + '.txt', "file alternate outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=html', type + '.html', "html5"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=epub', type + '.epub', "book"))
+          break;
+        case "spreadsheets":
+          linkPrefix = "https://docs.google.com/spreadsheets/d/"
+
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/edit?usp=sharing', type + '.share', "linkify"))
+
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/preview', type + '.preview'))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/copy', type + '.copy', "copy"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=pdf', type + '.pdf', "file pdf outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=xlsx', type + '.xlsx', "file excel outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=ods', type + '.ods', "file alternate"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=csv', type + '.csv', "file alternate outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=tsv', type + '.tsc', "file alternate outline"))
+          break;
+        case "presentation":
+          linkPrefix = "https://docs.google.com/spreadsheets/d/"
+
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/edit?usp=sharing', type + '.share', "linkify"))
+
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/preview', type + '.preview', "play circle"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/present', type + '.present', "play circle"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/copy', type + '.copy', "copy"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=pdf', type + '.pdf', "file pdf outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=pptx', type + '.pptx', "file powerpoint outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=odp', type + '.odp', "file alternate"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=txt', type + '.txt', "file alternate outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=jpeg', type + '.jpeg', "file image outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=png', type + '.png', "file image outline"))
+          links.push(this.buildGoogleDocLink(linkPrefix + id + '/export?format=svg', type + '.svg', "file image outline"))
+          break;
+        default:
+          // https://drive.google.com/open?id=0B3_qgRmbvvZ1MTc4M0NRZ25CSFRPQ3NMS1RXdk1PSG52VXVr
+          links.push(this.buildGoogleDocLink('https://drive.google.com/open?id=' + id, type + '.share', "linkify"))
+
+          links.push(this.buildGoogleDocLink("https://drive.google.com/uc?export=download&id=" + id, type + '.download', "file alternate outline"))
+      }
+      return links
     }
-  }
+    
+  },
+  
 }
 
 export default config
