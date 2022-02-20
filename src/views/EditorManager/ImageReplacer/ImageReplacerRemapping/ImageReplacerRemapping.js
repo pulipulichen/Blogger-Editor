1ec8165e0e2f3e9daa9ba9d6ee99df69258cc4f0
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-02-20 20:50:38 +0800

    0220 Image Replace for the new URL of Blogger's images 還有好多麻煩的事情...可不能一直卡在這裡...

--- src/views/EditorManager/ImageReplacer/ImageReplacerRemapping/ImageReplacerRemapping.js
+++ src/views/EditorManager/ImageReplacer/ImageReplacerRemapping/ImageReplacerRemapping.js
@@ -1,4 +1,6 @@
-/* global VueHelper, DayjsHelper, EventManager, FunctionHelper, BloggerImageHelper, FileSystemHelper, WindowHelper */
+/* global VueHelper, DayjsHelper, EventManager, FunctionHelper, BloggerImageHelper, FileSystemHelper, WindowHelper, FieldPostBody */
+
+import FieldPostBody from './../../FieldPostBody.js'
 
 var config = {
   //name: "main-content",
@@ -8,16 +10,31 @@ var config = {
   ],
   data: function () {
     return {
-      
+      imageRemapList: [],
+      postBodyImageList: [],
+      currentPairingItem: null,
+      selectedImageList: []
     }
   },
   mounted: function () {
     
   },
+  watch: {
+    imageHTML () {
+      this.imageRemapList = this.htmlImageList.map(url => {
+        return {
+          url,
+          mapPostBodyImage: null
+        }
+      })
+      
+      this.buildFieldPostBodyImageList()
+    }
+  },
   computed: {
     htmlImageList () {
       let html = this.imageHTML
-      console.log(html)
+      //console.log(html)
       if (!html) {
         return []
       }
@@ -29,6 +46,7 @@ var config = {
       images.find('a[href] > img[src]').each((i, ele) => {
         list.push(ele.src)
       })
+      
       return list
     }
   },
@@ -36,7 +54,58 @@ var config = {
     
   },
   methods: {
-    
+    buildFieldPostBodyImageList () {
+      let list = FieldPostBody.getImageList()
+      
+      this.postBodyImageList = []
+      list.map(url => {
+        if (!url.startsWith('filesystem:')) {
+          return false
+        }
+        
+        let filename = url.slice(url.lastIndexOf('/') + 1)
+        this.postBodyImageList.push({
+          filename,
+          url
+        })
+      })
+      
+      return true
+    },
+    getFilename (url) {
+      return url.slice(url.lastIndexOf('/') + 1)
+    },
+    selectToPair (i) {
+      if (this.currentPairingItem === i) {
+        this.currentPairingItem = null
+        return false
+      }
+      
+      this.currentPairingItem = i
+    },
+    toPair (url) {
+      if (this.currentPairingItem === null) {
+        return false
+      }
+      
+      this.imageRemapList[this.currentPairingItem].mapPostBodyImage = url
+      
+      this.currentPairingItem = null
+      this.updateSelectedImageList()
+    },
+    updateSelectedImageList () {
+      this.selectedImageList = []
+      this.imageRemapList.forEach(item => {
+        if (item.mapPostBodyImage
+                && this.selectedImageList.indexOf(item.mapPostBodyImage) === -1) {
+          this.selectedImageList.push(item.mapPostBodyImage)
+        }
+      })
+      // selectedImageList
+    },
+    previewImage(url) {
+      WindowHelper.popup(url, this.getFilename(url))
+    }
   }
 }
 
